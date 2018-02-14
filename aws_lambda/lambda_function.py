a25bf7f775041f2c7f33837b7cf15b4ecff7bafe
Nitish Chauhan <nitish.nc18@gmail.com>
2018-02-15 01:54:09 +0530

    added zip file to use python imports

---
+++ aws_lambda/lambda_function.py
@@ -0,0 +1,107 @@
+import urllib2
+import json
+import requests
+
+def lambda_handler(event, context):
+    if (event["session"]["application"]["applicationId"] !=
+            "amzn1.ask.skill.f4f81b69-b764-4e33-a102-18f19e46bd9b"):
+        raise ValueError("Invalid Application ID")
+    
+    if event["session"]["new"]:
+        on_session_started({"requestId": event["request"]["requestId"]}, event["session"])
+
+    if event["request"]["type"] == "LaunchRequest":
+        return on_launch(event["request"], event["session"])
+    elif event["request"]["type"] == "IntentRequest":
+        return on_intent(event["request"], event["session"])
+    elif event["request"]["type"] == "SessionEndedRequest":
+        return on_session_ended(event["request"], event["session"])
+
+def on_session_started(session_started_request, session):
+    print "Starting new session."
+
+def on_launch(launch_request, session):
+    return get_welcome_response()
+
+def on_intent(intent_request, session):
+    intent = intent_request["intent"]
+    intent_name = intent_request["intent"]["name"]
+
+    if intent_name == "price_bitcoin":
+        return get_bitcoin_price()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+def on_session_ended(session_ended_request, session):
+    print "Ending session."
+    # Cleanup goes here...
+
+def handle_session_end_request():
+    card_title = "BART - Thanks"
+    speech_output = "Thank you for using the BART skill.  See you next time!"
+    should_end_session = True
+
+    return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+
+def get_welcome_response():
+    session_attributes = {}
+    card_title = "BART"
+    speech_output = "Welcome to the Alexa Crypto Updater skill. " \
+                    "You can ask me about prices of different Crypto Currencies" 
+                    
+    reprompt_text = "Please ask me about price of different coins" \
+                    "for example Bitcoin."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_bitcoin_price():
+    session_attributes = {}
+    card_title = "Crypto Updater Status"
+    reprompt_text = ""
+    should_end_session = False
+
+    API_URL = 'https://chasing-coins.com/api/v1/std/coin/BTC'
+    api_resp = {}
+    btc_price = "NA"
+    try:
+        api_resp = requests.get(API_URL)
+        api_json_body = api_resp.json()
+        btc_price = api_json_body.get('price',"NA")
+    except:
+        pass    
+    speech_output = "The price of one bitcoin is" + btc_price + "dollars"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        "outputSpeech": {
+            "type": "PlainText",
+            "text": output
+        },
+        "card": {
+            "type": "Simple",
+            "title": title,
+            "content": output
+        },
+        "reprompt": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": reprompt_text
+            }
+        },
+        "shouldEndSession": should_end_session
+    }
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        "version": "1.0",
+        "sessionAttributes": session_attributes,
+        "response": speechlet_response
+    }
