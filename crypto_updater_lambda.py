194cb921bbb9327c9768e9013095d193f67082e7
Nitish Chauhan <nitish.nc18@gmail.com>
2018-02-25 23:20:20 +0530

    added support for multiple coins | made it generic using custom slots

--- crypto_updater_lambda.py
+++ crypto_updater_lambda.py
@@ -27,8 +27,14 @@ def on_intent(intent_request, session):
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
 
-    if intent_name == "price_bitcoin":
-        return get_bitcoin_price()
+    # custom intents
+    if intent_name == "intro":
+        return get_intro()
+
+    elif intent_name == "coin_price":
+        return get_coin_price(intent)     
+        
+    # predefined intents
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -41,15 +47,14 @@ def on_session_ended(session_ended_request, session):
     # Cleanup goes here...
 
 def handle_session_end_request():
-    card_title = "BART - Thanks"
-    speech_output = "Thank you for using the BART skill.  See you next time!"
+    card_title = "CRYPTOUPDATER - Thanks"
+    speech_output = "Thank you for using the Crypto Updater skill.  See you next time!"
     should_end_session = True
-
     return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
 
 def get_welcome_response():
     session_attributes = {}
-    card_title = "BART"
+    card_title = "CRYPTOUPDATER"
     speech_output = "Welcome to the Alexa Crypto Updater skill. " \
                     "You can ask me about prices of different Crypto Currencies" 
                     
@@ -59,24 +64,72 @@ def get_welcome_response():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-def get_bitcoin_price():
+
+def get_intro():
     session_attributes = {}
     card_title = "Crypto Updater Status"
     reprompt_text = ""
     should_end_session = False
 
-    API_URL = 'https://chasing-coins.com/api/v1/std/coin/BTC'
-    api_resp = {}
-    btc_price = None
-    try:
-        api_resp = requests.get(API_URL)
-    except:
-        pass    
-    btc_price = api_resp.get('price','NA')
-    speech_output = "The price of one bitcoin is" + btc_price + "dollars"
+    speech_output = "Hello, cryptoupdater will basically tell you about latest prices of different crypto currencies like Bitcoin, Etherium, litecoin, etc. You just have to tell coin's name and it will fetch you the latest price in US Dollars."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+
+def get_coin_price(intent):
+    session_attributes = {}
+    card_title = "Crypto Updater Status"
+    speech_output = "Sorry, could not get you ! Can you please try again !! Try speaking name of the coin of which you want to know the price."
+    reprompt_text = "Sorry, could not get you ! Can you please try again !! Try speaking name of the coin of which you want to know the price."
+    should_end_session = False
+
+    API_URL = 'https://chasing-coins.com/api/v1/std/coin/'
+
+    if "coin" in intent["slots"]:
+        coin_name = intent["slots"]["coin"]["value"]
+        coin_code = get_coin_code(coin_name)
+        if coin_code:
+            API_URL += str(coin_code)
+            api_resp = {}
+            coin_price = "NA"
+            try:
+                api_resp = requests.get(API_URL)
+                api_json_body = api_resp.json()
+                coin_price = api_json_body.get('price',"NA")
+            except:
+                pass 
+            speech_output = "The price of one" + coin_name + 'is' + coin_price + " dollars"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def coin_code(coin_name):
+    # mapping between coin name & it's code
+    coin_mapping = {
+      "VIBE" : "VIBE",
+      "Request Network" : "REQ",
+      "Power Ledger" : "POWR",
+      "BitShares" : "BTS",
+      "Tether" : "USDT",
+      "Bitcoin Gold" : "BTG",
+      "NEM" : "XEM",
+      "TRON" : "TRX ",
+      "VeChain" : "VEN",
+      "Dash" : "DASH",
+      "Monero" : "XMR",
+      "IOTA" : "MIOTA",
+      "Stellar" : "XLM",
+      "NEO" : "NEO",
+      "Cardano" : "ADA",
+      "Litecoin" : "LTC",
+      "Bitcoin cash" : "BCH",
+      "Ripple" : "XRP",
+      "Ethereum" : "ETH ",
+      "Bitcoin" : "BTC"
+    }
+    if str(coin_name) in coin_mapping:
+        return coin_mapping[coin_name]
+    return None    
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
